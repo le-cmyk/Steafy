50d83861f54f7a4b3578d34bc03c3969712c41ed
Louis <louis.dupand@gmail.com>
2022-11-09 10:01:00 +0100

    Steafy parts

--- Steafy/src/app/app.module.ts
+++ Steafy/src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RouterModule, UrlSegment } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+/* . . . */
+    ])],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
