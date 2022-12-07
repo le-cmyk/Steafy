6ba94e7a27b04d58239c4927d5ae9163ca7f8fc6
William GAINNIER <william.gainnier@edu.devinci.fr>
2022-12-07 14:58:32 +0100

    transfer home - score

--- src/app/score-moniteur/score-moniteur.component.ts
+++ src/app/score-moniteur/score-moniteur.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-score-moniteur',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScoreMoniteurComponent implements OnInit {
 
-  constructor() { }
+  public tags: any;
+  public home: any; 
+  title = 'Changement Scores';
+   angForm: FormGroup;
+  constructor(
+    public httpClient : HttpClient,
+    public fb: FormBuilder
+  ){this.createForm();}
 
   ngOnInit(): void {
+    this.getMonitor();
+  }
+
+  getMonitor(){
+    this.httpClient.get<any>('http://localhost:3000/home').subscribe(
+      response => {
+        console.log()
+        this.home = response;
+       
+      }
+    );
+  }
+
+  createForm() {
+    this.angForm = this.fb.group({
+       name: ['', Validators.required ],
+       address: ['', Validators.required ],
+    });
   }
 
 }
